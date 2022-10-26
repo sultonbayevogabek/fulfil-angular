f36c8024f7efd1383f630ef2b26c6f271e0c8d73
sultonbayevogabek <sultonbayevogabek@gmail.com>
2022-10-26 19:10:10 +0500

    Finish responsive

---
+++ src/app/admin/cabinet/header/header.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { ApiService } from '../../shared/services/api.service'
+
+@Component({
+   selector: 'app-header',
+   templateUrl: './header.component.html'
+})
+
+export class HeaderComponent implements OnInit {
+   form: FormGroup
+   created = false
+
+   constructor(
+      private _adminApiService: ApiService
+   ) {}
+
+   ngOnInit(): void {
+      this._adminApiService.getHeader()
+         .subscribe(res => {
+            this.form.setValue({
+               phoneNumber: res.phoneNumber,
+               students: res.students,
+               teachers: res.teachers,
+               lessons: res.lessons ?? null,
+               youtube: res.youtube
+            })
+            this.created = true
+         })
+      this.form = new FormGroup({
+         phoneNumber: new FormControl(null, [
+            Validators.required,
+            Validators.minLength(12),
+            Validators.maxLength(12)
+         ]),
+         students: new FormControl(null, Validators.required),
+         teachers: new FormControl(null, Validators.required),
+         lessons: new FormControl(null, Validators.required),
+         youtube: new FormControl(null, Validators.required)
+      })
+   }
+
+   onSubmit() {
+      if (this.form.invalid) return
+
+      if (this.created) {
+         this._adminApiService.updateHeader(this.form.value)
+            .subscribe(_ => {
+               window.alert(`O'zgarishlar muvaffaqqiyatli amalga oshirildi`)
+            }, _ => {
+               window.alert(`Xatolik ro'y berdi :(`)
+            })
+      }
+   }
+}
